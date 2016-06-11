d93554a9b380a6ab74646ae0123e245cc37b0419
Matt Sweeney <matt.sweeney@yahoo.com>
2016-06-10 18:24:55 -0700

    Akai-AMX: Clean up lint and minor reformatting.  No functional changes.

--- res/controllers/Akai-AMX-scripts.js
+++ res/controllers/Akai-AMX-scripts.js
@@ -1,46 +1,46 @@
-	
+/*      This mapping is still being worked on and is not finsihed
+ *      Author: Prism with help from Be
+ *      Mixxx Version: 1.12.0-beta1 (build 1.12 r5442)
+ *      System: Ubuntu 15.04
+ *      @see https://www.mixxx.org/forums/viewtopic.php?f=7&t=7514 
+ */
 
 function AMX() {}
-
-/*      This mapping is still being worked on and is not finsihed
-*       Author: Prism with help from Be
-*       Mixxx Version: 1.12.0-beta1 (build 1.12 r5442)
-*       System: Ubuntu 15.04
-*/
-
 
 AMX.shift = false;
 
 AMX.main = true;
 
 AMX.masterVuMeters = {
-    "VuMeterL": {"ch": 0xB0, "midino": 0x3E},
-    "VuMeterR": {"ch": 0xB0, "midino": 0x3F}
-}
+    'VuMeterL': {'ch': 0xB0, 'midino': 0x3E},
+    'VuMeterR': {'ch': 0xB0, 'midino': 0x3F}
+};
 
 AMX.channelVuMeters = {
-    "[Channel1]": {"ch": 0xB0, "midino": 0x40},
-    "[Channel2]": {"ch": 0xB0, "midino": 0x41}
-}
+    '[Channel1]': {'ch': 0xB0, 'midino': 0x40},
+    '[Channel2]': {'ch': 0xB0, 'midino': 0x41}
+};
 
 AMX.playLeds = {
-    "[Channel1]": {"ch": 0x90, "midino": 0x0A},
-    "[Channel2]": {"ch": 0x90, "midino": 0x0B}
-}
+    '[Channel1]': {'ch': 0x90, 'midino': 0x0A},
+    '[Channel2]': {'ch': 0x90, 'midino': 0x0B}
+};
+
 AMX.cueLeds = {
-    "[Channel1]": {"ch": 0x90, "midino": 0x08},
-    "[Channel2]": {"ch": 0x90, "midino": 0x09}
-}
-AMX.init = function (id, debug) { // called when the device is opened & set up
-        //connect VUmeters
-    engine.connectControl("[Master]", "VuMeterL", "AMX.volumeLEDs");
-    engine.connectControl("[Master]", "VuMeterR", "AMX.volumeLEDs");
-    engine.connectControl("[Channel1]", "VuMeter", "AMX.volumeLEDs");
-    engine.connectControl("[Channel2]", "VuMeter", "AMX.volumeLEDs");
-    engine.connectControl("[Channel1]", "play_indicator", "AMX.onIndicators");
-    engine.connectControl("[Channel2]", "play_indicator", "AMX.onIndicators");
-    engine.connectControl("[Channel1]", "cue_indicator", "AMX.onIndicators");
-    engine.connectControl("[Channel2]", "cue_indicator", "AMX.onIndicators");
+    '[Channel1]': {'ch': 0x90, 'midino': 0x08},
+    '[Channel2]': {'ch': 0x90, 'midino': 0x09}
+};
+
+AMX.init = function(id, debug) { // called when the device is opened & set up
+    //connect VUmeters
+    engine.connectControl('[Master]', 'VuMeterL', 'AMX.volumeLEDs');
+    engine.connectControl('[Master]', 'VuMeterR', 'AMX.volumeLEDs');
+    engine.connectControl('[Channel1]', 'VuMeter', 'AMX.volumeLEDs');
+    engine.connectControl('[Channel2]', 'VuMeter', 'AMX.volumeLEDs');
+    engine.connectControl('[Channel1]', 'play_indicator', 'AMX.onIndicators');
+    engine.connectControl('[Channel2]', 'play_indicator', 'AMX.onIndicators');
+    engine.connectControl('[Channel1]', 'cue_indicator', 'AMX.onIndicators');
+    engine.connectControl('[Channel2]', 'cue_indicator', 'AMX.onIndicators');
 
 
     midi.sendShortMsg(0x90,0x06,0x00); //Sync Channel 1 
@@ -58,18 +58,18 @@ AMX.init = function (id, debug) { // called when the device is opened & set up
     midi.sendShortMsg(0x90,0x0D,0x00); //PFL Channel 2
 };
 
-AMX.shutdown = function () {
-        // clear VU meter LEDs
+AMX.shutdown = function() {
+    // clear VU meter LEDs
     midi.sendShortMsg(AMX.masterVuMeters.VuMeterL.ch, AMX.masterVuMeters.VuMeterL.midino, 0);
     midi.sendShortMsg(AMX.masterVuMeters.VuMeterR.ch, AMX.masterVuMeters.VuMeterR.midino, 0);
 
 
-    for(var i=1; i<=2; i++) {
-        midi.sendShortMsg(AMX.channelVuMeters["[Channel"+i+"]"].ch, AMX.channelVuMeters["[Channel"+i+"]"].midino, 0);
+    for (var i = 1; i <= 2; i++) {
+        midi.sendShortMsg(AMX.channelVuMeters['[Channel'+i+']'].ch, AMX.channelVuMeters['[Channel'+i+']'].midino, 0);
     }
 
-    //midi.sendShortMsg(AMX.channelVuMeters["[Channel1]"].ch, AMX.channelVuMeters["[Channel1]"].midino, 0);
-    //midi.sendShortMsg(AMX.channelVuMeters["[Channel2]"].ch, AMX.channelVuMeters["[Channel2]"].midino, 0);
+    //midi.sendShortMsg(AMX.channelVuMeters['[Channel1]'].ch, AMX.channelVuMeters['[Channel1]'].midino, 0);
+    //midi.sendShortMsg(AMX.channelVuMeters['[Channel2]'].ch, AMX.channelVuMeters['[Channel2]'].midino, 0);
     midi.sendShortMsg(0x90,0x06,0x00); //Sync Channel 1 
     midi.sendShortMsg(0x90,0x07,0x00); //Sync Channel 2
     midi.sendShortMsg(0x90,0x0C,0x00); //PFL Channel 1
@@ -86,12 +86,11 @@ AMX.shutdown = function () {
     midi.sendShortMsg(0x90,0x0D,0x00); //PFL Channel 2
 };
 
-AMX.volumeLEDs = function (value, group, control) {
-    value=(value*85);
-    var ch;
-    var midino;
+AMX.volumeLEDs = function(value, group, control) {
+    value *= 85;
+    var ch, midino;
 
-    if (group == "[Master]") {
+    if (group === '[Master]') {
         ch = AMX.masterVuMeters[control].ch;
         midino = AMX.masterVuMeters[control].midino;
     } else {
@@ -101,25 +100,25 @@ AMX.volumeLEDs = function (value, group, control) {
     midi.sendShortMsg(ch, midino, value);
 };
 
-AMX.xfaderCurve = function (channel, control, value, status, group) {
+AMX.xfaderCurve = function(channel, control, value, status, group) {
     script.crossfaderCurve(value);
 };
 
 AMX.shiftButton = function(channel, control, value, status, group) {
-        AMX.shift = !AMX.shift;
-        print(AMX.shift);
-}
+    AMX.shift = !AMX.shift;
+    print(AMX.shift);
+};
 
-AMX.onIndicators = function (value, group, control) {
-    if(control == 'play_indicator') {
+AMX.onIndicators = function(value, group, control) {
+    if (control === 'play_indicator') {
         midi.sendShortMsg(AMX.playLeds[group].ch, AMX.playLeds[group].midino, value);
     }
-    if(control == 'cue_indicator') {
+    if (control === 'cue_indicator') {
         midi.sendShortMsg(AMX.cueLeds[group].ch, AMX.cueLeds[group].midino, value);
     }
-}
+};
 
-AMX.scroller = function (channel, control, value, status, group) {
+AMX.scroller = function(channel, control, value, status, group) {
     
     var direction = {
         'tracks': { 'up': 'SelectPrevTrack', 'down': 'SelectNextTrack'},
@@ -127,17 +126,17 @@ AMX.scroller = function (channel, control, value, status, group) {
     };
     var list = AMX.main ? 'tracks' : 'library';
 
-    if(value == 1) {
+    if (value === 1) {
         engine.setValue(group, direction[list].down, 1);
     } else {
         engine.setValue(group, direction[list].up, 1);
     }
-}
+};
 
 AMX.libraryToggle = function(channel, control, value, status, group) {
-    if(AMX.shift === true) {
+    if (AMX.shift) {
         engine.setValue('[Playlist]', 'ToggleSelectedSidebarItem', 1);
     } else {
         AMX.main = !AMX.main;
     }
-}
+};
