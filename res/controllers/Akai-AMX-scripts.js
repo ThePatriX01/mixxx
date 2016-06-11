9a49ee410e78a7a52eea7f4dc7bdf05f048f4860
Matt Sweeney <matt.sweeney@yahoo.com>
2016-06-10 18:58:55 -0700

    Akai-AMX: Add mappings for search, pitch, and xfade reverse controls.     * Map browse+search to channel search     * Map browse+search+shift to granular channel search     * Map gain+shift to channel speed control     * Map xfade reverse switch to fader reverse     * Update shift to apply only while pressed     * Update crossFaderCurve call with min and max args     * Add pregain script

--- res/controllers/Akai-AMX-scripts.js
+++ res/controllers/Akai-AMX-scripts.js
@@ -1,13 +1,22 @@
-/*      This mapping is still being worked on and is not finsihed
- *      Author: Prism with help from Be
- *      Mixxx Version: 1.12.0-beta1 (build 1.12 r5442)
- *      System: Ubuntu 15.04
- *      @see https://www.mixxx.org/forums/viewtopic.php?f=7&t=7514 
+/*
+ * This mapping is still being worked on and is not finished.
+ * Author: Prism with help from Be
+ * Mixxx Version: 1.12.0-beta1 (build 1.12 r5442)
+ * System: Ubuntu 15.04
+ * @see https://www.mixxx.org/forums/viewtopic.php?f=7&t=7514
  */
 
 function AMX() {}
 
+AMX.SEARCH_STEP = 0.05;
+AMX.SEARCH_STEP_SHIFTED = 0.01;
+
+AMX.GAIN_STEP = 0.05;
+
 AMX.shift = false;
+
+AMX.search1 = false;
+AMX.search2 = false;
 
 AMX.main = true;
 
@@ -41,7 +50,6 @@ AMX.init = function(id, debug) { // called when the device is opened & set up
     engine.connectControl('[Channel2]', 'play_indicator', 'AMX.onIndicators');
     engine.connectControl('[Channel1]', 'cue_indicator', 'AMX.onIndicators');
     engine.connectControl('[Channel2]', 'cue_indicator', 'AMX.onIndicators');
-
 
     midi.sendShortMsg(0x90,0x06,0x00); //Sync Channel 1 
     midi.sendShortMsg(0x90,0x07,0x00); //Sync Channel 2
@@ -101,12 +109,22 @@ AMX.volumeLEDs = function(value, group, control) {
 };
 
 AMX.xfaderCurve = function(channel, control, value, status, group) {
-    script.crossfaderCurve(value);
+    script.crossfaderCurve(value, 0, 127);
 };
 
 AMX.shiftButton = function(channel, control, value, status, group) {
     AMX.shift = !AMX.shift;
-    print(AMX.shift);
+    print('shift: ' + AMX.shift);
+};
+
+AMX.searchButton = function(channel, control, value, status, group) {
+    if (control === 2) {
+        AMX.search1 = !AMX.search1;
+        print('search1: ' + AMX.search1);
+    } else {
+        AMX.search2 = !AMX.search2;
+        print('search2: ' + AMX.search2);
+    }
 };
 
 AMX.onIndicators = function(value, group, control) {
@@ -119,17 +137,24 @@ AMX.onIndicators = function(value, group, control) {
 };
 
 AMX.scroller = function(channel, control, value, status, group) {
-    
-    var direction = {
-        'tracks': { 'up': 'SelectPrevTrack', 'down': 'SelectNextTrack'},
-        'library': { 'up': 'SelectPrevPlaylist', 'down': 'SelectNextPlaylist'}
-    };
-    var list = AMX.main ? 'tracks' : 'library';
 
-    if (value === 1) {
-        engine.setValue(group, direction[list].down, 1);
+    // Search on scroll if either search button is pressed. 
+    if (AMX.search1 || AMX.search2) {
+        AMX.searchChannel.apply(null, arguments);
+
+    // Scroll through tracks and directories.
     } else {
-        engine.setValue(group, direction[list].up, 1);
+        var direction = {
+            'tracks': { 'up': 'SelectPrevTrack', 'down': 'SelectNextTrack'},
+            'library': { 'up': 'SelectPrevPlaylist', 'down': 'SelectNextPlaylist'}
+        };
+        var list = AMX.main ? 'tracks' : 'library';
+
+        if (value === 1) {
+            engine.setValue(group, direction[list].down, 1);
+        } else {
+            engine.setValue(group, direction[list].up, 1);
+        }
     }
 };
 
@@ -138,5 +163,48 @@ AMX.libraryToggle = function(channel, control, value, status, group) {
         engine.setValue('[Playlist]', 'ToggleSelectedSidebarItem', 1);
     } else {
         AMX.main = !AMX.main;
+    }
+};
+
+AMX.offsetPlayPosition = function(channel, offset) {
+    var position = engine.getValue(channel, 'playposition') + offset;
+    if (position < 0) position = 0;
+    if (position > 1) position = 1;
+
+    engine.setValue(channel, 'playposition', position);
+};
+
+AMX.searchChannel = function(channel, control, value, status, group) {
+    // Do a more granular search when holding shift.
+    var step = !AMX.shift ? AMX.SEARCH_STEP : AMX.SEARCH_STEP_SHIFTED;
+
+    // Scrolling forward is `1`, and backward is `127`.
+    if (value === 127) step = -step;
+
+    if (AMX.search1) {
+        AMX.offsetPlayPosition('[Channel1]', step);
+    }
+
+    // No `else if` to allow both channels to be searched together.
+    if (AMX.search2) {
+        AMX.offsetPlayPosition('[Channel2]', step);
+    }
+};
+
+AMX.pitchShift = function(channel, control, value, status, group) {
+    var action = (value < 100) ? 'rate_up_small' : 'rate_down_small';
+    engine.setValue(group, action, 1);
+};
+
+AMX.pregain = function(channel, control, value, status, group) {
+    // Use gain knob for pitch control when shift is pressed.
+    if (AMX.shift) {
+        AMX.pitchShift.apply(null, arguments);
+    } else {
+        // The `value` can vary slightly depending on knob twist speed,
+        // but it always above 100 if going forward (usually 127).
+        var step = (value < 100) ? AMX.GAIN_STEP : -AMX.GAIN_STEP;
+        var gain = engine.getValue(group, 'pregain') + step;
+        engine.setValue(group, 'pregain', gain);
     }
 };
